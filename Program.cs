00e4ab23af966911a3b8995efc86edf704c3b596
Qiyang Lu <yanmofeixi@gmail.com>
2023-03-16 21:11:07 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DesktopApp
+{
+    internal partial class Program
+    {
+        private static readonly NotifyIcon icon = new()
+        {
+            Text = "TurnOffDisplay",
+            Icon = new Icon("icon.ico"),
+            Visible = true
+        };
+        private static readonly ContextMenuStrip contextMenuStrip = new();
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        private static readonly int WM_SYSCOMMAND = 0x0112;
+        private static readonly int SC_MONITORPOWER = 0xF170;
+        private static readonly int MONITOR_ON = -1;
+        private static readonly int MONITOR_OFF = 2;
+
+        private static void Main(string[] args)
+        {
+            SetUpTrayIcon();
+            SetUpTcpListener();
+            Application.Run();
+        }
+
+        private static void AddMenuItem(string text, EventHandler clickHandler)
+        {
+            var menuItem = new ToolStripMenuItem
+            {
+                Text = text
+            };
+            menuItem.Click += clickHandler;
+            contextMenuStrip.Items.Add(menuItem);
+        }
+
+        private static void SetUpTrayIcon()
+        {
+            AddMenuItem("Turn Off Display", TurnOffDisplayToolStripMenuItem_Click);
+            AddMenuItem("Turn On Display", TurnOnDisplayToolStripMenuItem_Click);
+            AddMenuItem("Exit", ExitToolStripMenuItem_Click);
+            icon.ContextMenuStrip = contextMenuStrip;
+            icon.MouseClick += new MouseEventHandler(Icon_MouseClick);
+        }
+
+        private static void SetUpTcpListener()
+        {
+            var listenerThread = new Thread(() =>
+            {
+                var listener = new TcpListener(IPAddress.Any, 12345);
+                listener.Start();
+                while (true)
+                {
+                    var client = listener.AcceptTcpClient();
+                    var stream = client.GetStream();
+                    var buffer = new byte[1024];
+                    var bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    var message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    if (message == "turn off")
+                    {
+                        TurnOffDisplay();
+                    }
+                    stream.Close();
+                    client.Close();
+                }
+            })
+            {
+                IsBackground = true
+            };
+            listenerThread.Start();
+        }
+
+        private static void ExitToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            CleanUpNotifyIcon();
+            Application.Exit();
+        }
+
+        private static void Icon_MouseClick(object? sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && contextMenuStrip != null)
+            {
+                contextMenuStrip.Show(Control.MousePosition);
+            }
+        }
+
+        private static void TurnOffDisplayToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            TurnOffDisplay();
+        }
+
+        private static void TurnOnDisplayToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            TurnOnDisplay();
+        }
+
+        private static void OnApplicationExit(object sender, EventArgs e)
+        {
+            icon.Visible = false;
+        }
+
+        private static void CleanUpNotifyIcon()
+        {
+            icon.Visible = false;
+            icon.Dispose();
+        }
+
+        private static void TurnOffDisplay()
+        {
+            Task.Run(() =>
+            {
+                _ = SendMessage(0xFFFF, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF);
+            });
+        }
+
+        private static void TurnOnDisplay()
+        {
+            Task.Run(() =>
+            {
+                _ = SendMessage(0xFFFF, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_ON);
+            });
+        }
+
+    }
+}
---
+++ TitleBarRemover.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace TurnOffDisplay
+{
+    public static partial class TitleBarRemover
+    {
+        // Win32 API declarations
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,
+            int X, int Y, int cx, int cy, uint uFlagske);
+
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_FRAMECHANGED = 0x0020;
+        private const int GWL_STYLE = -16;
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_CAPTION = 0x00C00000;
+        private const int WS_SYSMENU = 0x00080000;
+        private const int WS_THICKFRAME = 0x00040000;
+        private const int WS_MINIMIZE = 0x20000000;
+        private const int WS_MAXIMIZEBOX = 0x00010000;
+        private const long WS_EX_DLGMODALFRAME = 0x0001L;
+
+        // Method to remove title bar of another program
+        private static void RemoveTitleBar(string executablePath)
+        {
+            var processes = Process.GetProcesses();
+            var hwnd = IntPtr.Zero;
+            foreach (var p in processes)
+            {
+                if (p.MainModule != null && p.MainModule.FileName == executablePath)
+                {
+                    hwnd = p.MainWindowHandle;
+                    break;
+                }
+            }
+            // Get current style
+            long lCurStyle = GetWindowLong(hwnd, GWL_STYLE);
+            // Remove title bar elements
+            lCurStyle &= ~WS_CAPTION;
+            lCurStyle &= ~WS_SYSMENU;
+            lCurStyle &= ~WS_THICKFRAME;
+            lCurStyle &= ~WS_MINIMIZE;
+            lCurStyle &= ~WS_MAXIMIZEBOX;
+
+            // Apply new style
+            _ = SetWindowLong(hwnd, GWL_STYLE, (int)lCurStyle);
+            // Reapply a 3d border
+            lCurStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            _ = SetWindowLong(hwnd, GWL_EXSTYLE, (int)(lCurStyle | WS_EX_DLGMODALFRAME));
+            // Redraw
+            SetWindowPos(hwnd, IntPtr.Zero,
+                    0, 0,
+                    0,
+                    0,
+                    SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);
+        }
+    }
+}
